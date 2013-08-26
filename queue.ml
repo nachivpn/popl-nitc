54ee5d2055b83b97fe17207c19ac5f8b260b363a
nachivpn <nachivpn@gmail.com>
2013-08-26 22:45:16 +0530

    1st add

---
+++ queue.ml
@@ -0,0 +1,17 @@
+(*
+Functions to 
+	->add to 
+	->delete from
+	->print the contents 
+of a queue
+*)
+
+let enqueue q l = q::l;;
+
+let notempty li = if (li = []) then false else true;;
+
+let rec dequeue q = if((notempty q) && notempty(List.tl q)) then ((List.hd q) :: (dequeue (List.tl q))) else [];;
+
+let print q = List.map ret q;;
+
+
