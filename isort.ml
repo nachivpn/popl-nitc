a1499bcbb79575f75c39a0c103eaa70b4562e927
nachivpn <nachivpn@gmail.com>
2013-09-11 21:01:51 +0530

    completed with sorting

--- isort.ml
+++ isort.ml
@@ -6,3 +6,6 @@ let rec insert x li = match li with
 	[] -> [x]
 	| hd::tl -> if x < (List.hd li) then x::li else (List.hd li) :: insert x (List.tl li);;
 
+let rec dequeue q = if((notempty q) && notempty(List.tl q)) then ((List.hd q) :: (dequeue (List.tl q))) else [];;
+ 
+let rec sort q = if notempty q then (insert (last q) (sort (dequeue q))) else [];;   
