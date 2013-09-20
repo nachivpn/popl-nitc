630957c1cee4b9f4b8d99c38d394525382581e26
nachivpn <nachivpn@gmail.com>
2013-09-20 08:46:22 +0530

    added lex,yacc,syntax for 'and' (not working yet)

---
+++ utar_interpreter/parser.ml
@@ -0,0 +1,426 @@
+type token =
+  | IF of (Support.Error.info)
+  | THEN of (Support.Error.info)
+  | ELSE of (Support.Error.info)
+  | TRUE of (Support.Error.info)
+  | FALSE of (Support.Error.info)
+  | SUCC of (Support.Error.info)
+  | PRED of (Support.Error.info)
+  | ISZERO of (Support.Error.info)
+  | AND of (Support.Error.info)
+  | UCID of (string Support.Error.withinfo)
+  | LCID of (string Support.Error.withinfo)
+  | INTV of (int Support.Error.withinfo)
+  | FLOATV of (float Support.Error.withinfo)
+  | STRINGV of (string Support.Error.withinfo)
+  | APOSTROPHE of (Support.Error.info)
+  | DQUOTE of (Support.Error.info)
+  | ARROW of (Support.Error.info)
+  | BANG of (Support.Error.info)
+  | BARGT of (Support.Error.info)
+  | BARRCURLY of (Support.Error.info)
+  | BARRSQUARE of (Support.Error.info)
+  | COLON of (Support.Error.info)
+  | COLONCOLON of (Support.Error.info)
+  | COLONEQ of (Support.Error.info)
+  | COLONHASH of (Support.Error.info)
+  | COMMA of (Support.Error.info)
+  | DARROW of (Support.Error.info)
+  | DDARROW of (Support.Error.info)
+  | DOT of (Support.Error.info)
+  | EOF of (Support.Error.info)
+  | EQ of (Support.Error.info)
+  | EQEQ of (Support.Error.info)
+  | EXISTS of (Support.Error.info)
+  | GT of (Support.Error.info)
+  | HASH of (Support.Error.info)
+  | LCURLY of (Support.Error.info)
+  | LCURLYBAR of (Support.Error.info)
+  | LEFTARROW of (Support.Error.info)
+  | LPAREN of (Support.Error.info)
+  | LSQUARE of (Support.Error.info)
+  | LSQUAREBAR of (Support.Error.info)
+  | LT of (Support.Error.info)
+  | RCURLY of (Support.Error.info)
+  | RPAREN of (Support.Error.info)
+  | RSQUARE of (Support.Error.info)
+  | SEMI of (Support.Error.info)
+  | SLASH of (Support.Error.info)
+  | STAR of (Support.Error.info)
+  | TRIANGLE of (Support.Error.info)
+  | USCORE of (Support.Error.info)
+  | VBAR of (Support.Error.info)
+
+open Parsing;;
+let _ = parse_error;;
+# 7 "parser.mly"
+open Support.Error
+open Support.Pervasive
+open Syntax
+# 61 "parser.ml"
+let yytransl_const = [|
+    0|]
+
+let yytransl_block = [|
+  257 (* IF *);
+  258 (* THEN *);
+  259 (* ELSE *);
+  260 (* TRUE *);
+  261 (* FALSE *);
+  262 (* SUCC *);
+  263 (* PRED *);
+  264 (* ISZERO *);
+  265 (* AND *);
+  266 (* UCID *);
+  267 (* LCID *);
+  268 (* INTV *);
+  269 (* FLOATV *);
+  270 (* STRINGV *);
+  271 (* APOSTROPHE *);
+  272 (* DQUOTE *);
+  273 (* ARROW *);
+  274 (* BANG *);
+  275 (* BARGT *);
+  276 (* BARRCURLY *);
+  277 (* BARRSQUARE *);
+  278 (* COLON *);
+  279 (* COLONCOLON *);
+  280 (* COLONEQ *);
+  281 (* COLONHASH *);
+  282 (* COMMA *);
+  283 (* DARROW *);
+  284 (* DDARROW *);
+  285 (* DOT *);
+    0 (* EOF *);
+  286 (* EQ *);
+  287 (* EQEQ *);
+  288 (* EXISTS *);
+  289 (* GT *);
+  290 (* HASH *);
+  291 (* LCURLY *);
+  292 (* LCURLYBAR *);
+  293 (* LEFTARROW *);
+  294 (* LPAREN *);
+  295 (* LSQUARE *);
+  296 (* LSQUAREBAR *);
+  297 (* LT *);
+  298 (* RCURLY *);
+  299 (* RPAREN *);
+  300 (* RSQUARE *);
+  301 (* SEMI *);
+  302 (* SLASH *);
+  303 (* STAR *);
+  304 (* TRIANGLE *);
+  305 (* USCORE *);
+  306 (* VBAR *);
+    0|]
+
+let yylhs = "\255\255\
+\001\000\001\000\002\000\003\000\003\000\004\000\004\000\004\000\
+\004\000\004\000\005\000\005\000\005\000\005\000\000\000"
+
+let yylen = "\002\000\
+\001\000\003\000\001\000\001\000\006\000\001\000\002\000\002\000\
+\002\000\003\000\003\000\001\000\001\000\001\000\002\000"
+
+let yydefred = "\000\000\
+\000\000\000\000\000\000\012\000\013\000\000\000\000\000\000\000\
+\000\000\014\000\001\000\000\000\015\000\000\000\003\000\004\000\
+\006\000\000\000\007\000\008\000\009\000\000\000\000\000\000\000\
+\000\000\010\000\011\000\002\000\000\000\000\000\005\000"
+
+let yydgoto = "\002\000\
+\013\000\014\000\015\000\016\000\017\000"
+
+let yysindex = "\017\000\
+\001\000\000\000\255\254\000\000\000\000\005\255\005\255\005\255\
+\005\255\000\000\000\000\255\254\000\000\213\254\000\000\000\000\
+\000\000\017\255\000\000\000\000\000\000\005\255\233\254\001\000\
+\255\254\000\000\000\000\000\000\019\255\255\254\000\000"
+
+let yyrindex = "\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000"
+
+let yygindex = "\000\000\
+\255\255\000\000\009\000\000\000\007\000"
+
+let yytablesize = 295
+let yytable = "\003\000\
+\011\000\024\000\004\000\005\000\006\000\007\000\008\000\009\000\
+\004\000\005\000\010\000\018\000\019\000\020\000\021\000\022\000\
+\010\000\001\000\025\000\027\000\023\000\030\000\028\000\000\000\
+\000\000\000\000\000\000\000\000\026\000\000\000\000\000\000\000\
+\000\000\029\000\000\000\000\000\012\000\000\000\031\000\000\000\
+\000\000\000\000\012\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\003\000\000\000\000\000\004\000\005\000\006\000\007\000\
+\008\000\009\000\000\000\000\000\010\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\012\000"
+
+let yycheck = "\001\001\
+\000\000\045\001\004\001\005\001\006\001\007\001\008\001\009\001\
+\004\001\005\001\012\001\003\000\006\000\007\000\008\000\009\000\
+\012\001\001\000\002\001\043\001\012\000\003\001\024\000\255\255\
+\255\255\255\255\255\255\255\255\022\000\255\255\255\255\255\255\
+\255\255\025\000\255\255\255\255\038\001\255\255\030\000\255\255\
+\255\255\255\255\038\001\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\001\001\255\255\255\255\004\001\005\001\006\001\007\001\
+\008\001\009\001\255\255\255\255\012\001\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\038\001"
+
+let yynames_const = "\
+  "
+
+let yynames_block = "\
+  IF\000\
+  THEN\000\
+  ELSE\000\
+  TRUE\000\
+  FALSE\000\
+  SUCC\000\
+  PRED\000\
+  ISZERO\000\
+  AND\000\
+  UCID\000\
+  LCID\000\
+  INTV\000\
+  FLOATV\000\
+  STRINGV\000\
+  APOSTROPHE\000\
+  DQUOTE\000\
+  ARROW\000\
+  BANG\000\
+  BARGT\000\
+  BARRCURLY\000\
+  BARRSQUARE\000\
+  COLON\000\
+  COLONCOLON\000\
+  COLONEQ\000\
+  COLONHASH\000\
+  COMMA\000\
+  DARROW\000\
+  DDARROW\000\
+  DOT\000\
+  EOF\000\
+  EQ\000\
+  EQEQ\000\
+  EXISTS\000\
+  GT\000\
+  HASH\000\
+  LCURLY\000\
+  LCURLYBAR\000\
+  LEFTARROW\000\
+  LPAREN\000\
+  LSQUARE\000\
+  LSQUAREBAR\000\
+  LT\000\
+  RCURLY\000\
+  RPAREN\000\
+  RSQUARE\000\
+  SEMI\000\
+  SLASH\000\
+  STAR\000\
+  TRIANGLE\000\
+  USCORE\000\
+  VBAR\000\
+  "
+
+let yyact = [|
+  (fun _ -> failwith "parser")
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : Support.Error.info) in
+    Obj.repr(
+# 98 "parser.mly"
+      ( [] )
+# 294 "parser.ml"
+               :  Syntax.command list ))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'Command) in
+    let _2 = (Parsing.peek_val __caml_parser_env 1 : Support.Error.info) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 :  Syntax.command list ) in
+    Obj.repr(
+# 100 "parser.mly"
+      ( let cmd = _1 in
+          let cmds = _3 in
+          cmd::cmds )
+# 305 "parser.ml"
+               :  Syntax.command list ))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : 'Term) in
+    Obj.repr(
+# 107 "parser.mly"
+      ( (let t = _1 in Eval(tmInfo t,t)) )
+# 312 "parser.ml"
+               : 'Command))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : 'AppTerm) in
+    Obj.repr(
+# 111 "parser.mly"
+      ( _1 )
+# 319 "parser.ml"
+               : 'Term))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 5 : Support.Error.info) in
+    let _2 = (Parsing.peek_val __caml_parser_env 4 : 'Term) in
+    let _3 = (Parsing.peek_val __caml_parser_env 3 : Support.Error.info) in
+    let _4 = (Parsing.peek_val __caml_parser_env 2 : 'Term) in
+    let _5 = (Parsing.peek_val __caml_parser_env 1 : Support.Error.info) in
+    let _6 = (Parsing.peek_val __caml_parser_env 0 : 'Term) in
+    Obj.repr(
+# 113 "parser.mly"
+      ( TmIf(_1, _2, _4, _6) )
+# 331 "parser.ml"
+               : 'Term))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : 'ATerm) in
+    Obj.repr(
+# 117 "parser.mly"
+      ( _1 )
+# 338 "parser.ml"
+               : 'AppTerm))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 1 : Support.Error.info) in
+    let _2 = (Parsing.peek_val __caml_parser_env 0 : 'ATerm) in
+    Obj.repr(
+# 119 "parser.mly"
+      ( TmSucc(_1, _2) )
+# 346 "parser.ml"
+               : 'AppTerm))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 1 : Support.Error.info) in
+    let _2 = (Parsing.peek_val __caml_parser_env 0 : 'ATerm) in
+    Obj.repr(
+# 121 "parser.mly"
+      ( TmPred(_1, _2) )
+# 354 "parser.ml"
+               : 'AppTerm))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 1 : Support.Error.info) in
+    let _2 = (Parsing.peek_val __caml_parser_env 0 : 'ATerm) in
+    Obj.repr(
+# 123 "parser.mly"
+      ( TmIsZero(_1, _2) )
+# 362 "parser.ml"
+               : 'AppTerm))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : Support.Error.info) in
+    let _2 = (Parsing.peek_val __caml_parser_env 1 : 'ATerm) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'ATerm) in
+    Obj.repr(
+# 125 "parser.mly"
+      ( TmAnd(_1,_2) )
+# 371 "parser.ml"
+               : 'AppTerm))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : Support.Error.info) in
+    let _2 = (Parsing.peek_val __caml_parser_env 1 : 'Term) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : Support.Error.info) in
+    Obj.repr(
+# 130 "parser.mly"
+      ( _2 )
+# 380 "parser.ml"
+               : 'ATerm))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : Support.Error.info) in
+    Obj.repr(
+# 132 "parser.mly"
+      ( TmTrue(_1) )
+# 387 "parser.ml"
+               : 'ATerm))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : Support.Error.info) in
+    Obj.repr(
+# 134 "parser.mly"
+      ( TmFalse(_1) )
+# 394 "parser.ml"
+               : 'ATerm))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : int Support.Error.withinfo) in
+    Obj.repr(
+# 136 "parser.mly"
+      ( let rec f n = match n with
+              0 -> TmZero(_1.i)
+            | n -> TmSucc(_1.i, f (n-1))
+          in f _1.v )
+# 404 "parser.ml"
+               : 'ATerm))
+(* Entry toplevel *)
+; (fun __caml_parser_env -> raise (Parsing.YYexit (Parsing.peek_val __caml_parser_env 0)))
+|]
+let yytables =
+  { Parsing.actions=yyact;
+    Parsing.transl_const=yytransl_const;
+    Parsing.transl_block=yytransl_block;
+    Parsing.lhs=yylhs;
+    Parsing.len=yylen;
+    Parsing.defred=yydefred;
+    Parsing.dgoto=yydgoto;
+    Parsing.sindex=yysindex;
+    Parsing.rindex=yyrindex;
+    Parsing.gindex=yygindex;
+    Parsing.tablesize=yytablesize;
+    Parsing.table=yytable;
+    Parsing.check=yycheck;
+    Parsing.error_function=parse_error;
+    Parsing.names_const=yynames_const;
+    Parsing.names_block=yynames_block }
+let toplevel (lexfun : Lexing.lexbuf -> token) (lexbuf : Lexing.lexbuf) =
+   (Parsing.yyparse yytables 1 lexfun lexbuf :  Syntax.command list )
